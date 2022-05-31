29846141c83ed3ebebcd364d6921aa5c008c4449
Ryan Pascal <ryan.david.pascal@gmail.com>
2022-05-31 18:06:28 -0400

    Set up tailwind and some starter shit

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,36 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
+import adapterStatic from '@sveltejs/adapter-static';
+
+
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess({
+			postcss: true,
+		}),
+	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapterStatic({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: true
+		}),
+		/*
+		⚠️ You must ensure SvelteKit's trailingSlash option is set appropriately for your environment. If your host does not render /a.html upon receiving a request for /a then you will need to set trailingSlash: 'always' to create /a/index.html instead.
+		*/
+		trailingSlash: 'always',
+		// adapter: adapter(),
+		prerender: {
+			default: true,
+		}
 	}
 };
 
